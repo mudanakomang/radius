469f7e26716d3b16c67c4125132f3b8038ad5d0e
I Komang Mudana <mudanakomang@hotmail.com>
2020-10-16 23:20:04 +0800

    Ganti Logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ asset('dist/img/world.svg') }}" alt="Logo" class="brand-image img-circle "
+        <img src="{{ asset('dist/img/logo.png') }}" alt="Logo" class="brand-image img-circle "
              style="opacity: .8">
         <span class="brand-text font-weight-light"><br></span>
     </a>
